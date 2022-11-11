1f5ed28ccde1f7fe8e32a2aea82c1f703c2b6095
iamfre <zycs@yandex.ru>
2022-11-11 16:52:24 +0300

    make model nootebook

---
+++ database/migrations/2022_11_11_134542_create_notebooks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotebooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notebooks', function (Blueprint $table) {
+            $table->id();
+            $table->string('fio');
+            $table->string('company');
+            $table->string('phone')->unique();
+            $table->string('email')->unique();
+            $table->date('birthday');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notebooks');
+    }
+}
